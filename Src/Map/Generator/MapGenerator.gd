1d3e59e2db6aaf19d2da86db933fe9d631dbf84c
Juuso Ylikoski <jousimies2@gmail.com>
2023-02-04 01:16:10 +0200

    Fix code

--- Src/Map/Generator/MapGenerator.gd
+++ Src/Map/Generator/MapGenerator.gd
@@ -1,3 +1,5 @@
+extends Node
+
 const DESIRED_NODE_COUNT = 100
 const MIN_X = -100
 const MAX_X = 100
@@ -9,7 +11,9 @@ const MAX_EDGES = 5
 const MAX_NODE_LOOKUP = 10
 
 var rng = RandomNumberGenerator.new()
-
+func _ready():
+	generate_map()
+	
 func generate_map():
 	var map_state = MapState.new()
 	
@@ -29,17 +33,18 @@ func generate_nodes(map_state):
 		
 		var new_node = MapNode.new()
 		new_node.position = new_location
-		map_state.append(new_node)
+		map_state.nodes.append(new_node)
+		generated_nodes += 1
 		
 func generate_edges(map_state: MapState):
 	var nodes = map_state.nodes
 	
 	for node in nodes:
-		var node_edge_count = node.edges.count()
+		var node_edge_count = node.edges.size()
 		var edges_left = rng.randi_range(MIN_EDGES - node_edge_count, MAX_EDGES - node_edge_count)
 		
 		var closest_nodes = map_state.get_nodes_in_distance_order(node.position)
 		while edges_left > 0:
-			var node_to_be_added = closest_nodes[rng.randi(MAX_NODE_LOOKUP)]
+			var node_to_be_added = closest_nodes[rng.randi_range(0,MAX_NODE_LOOKUP)]
 			if node.add_edge(node_to_be_added):
 				edges_left -= 1
--- Src/Map/MapState.gd
+++ Src/Map/MapState.gd
@@ -3,19 +3,19 @@ class_name MapState
 var nodes = []
 
 func find_closest_node(position: Vector2):
-	if nodes.count() == 0:
-		return null
+	if nodes.size() == 0:
+		return INF
 	
 	var closest_distance = INF
 	var closest_node = nodes[0]
-	for node_index in range(1, nodes.count()):
+	for node_index in range(1, nodes.size()):
 		var node_candidate = nodes[node_index]
 		var distance = node_candidate.get_distance(position)
 		if (distance < closest_distance):
 			closest_distance = distance
 			closest_node = node_candidate
 			
-	return closest_node
+	return closest_distance
 	
 func get_nodes_in_distance_order(position: Vector2):
 	var distances_and_nodes = []
@@ -24,13 +24,13 @@ func get_nodes_in_distance_order(position: Vector2):
 		var distance = node.get_distance(position)
 		distances_and_nodes.append([distance, node])
 		
-	distances_and_nodes = distances_and_nodes.sort_custom(self, "distance_comparison")
+	distances_and_nodes.sort_custom(self, "distance_comparison")
 	
-	var nodes = []
+	var nodes2 = []
 	for node_and_distance in distances_and_nodes:
-		nodes.append(node_and_distance[1])
+		nodes2.append(node_and_distance[1])
 		
-	return nodes
+	return nodes2
 	
 func distance_comparison(distance_and_node_1, distance_and_node_2):
 	return distance_and_node_1[0] < distance_and_node_2[0]

bb797781ea8e875e9b9f9cec0c0b9968631da183
Appelsiini1 <37345715+Appelsiini1@users.noreply.github.com>
2023-02-03 23:01:37 +0200

    Add base variables and comments

--- Src/Classes/ShroomBase.gd
+++ Src/Classes/ShroomBase.gd
@@ -6,11 +6,19 @@ class_name Shroom_base
 
 var resource_points = 10
 var title = "Shroom"
+# Base values
 var base_draft_amount = 3
 var base_card_pick_amount = 1
+var base_expand_speed = 1
+var base_dmg = 0
+var base_node_health_change_speed = 0
+# Card arrays
 var cardpack_array = []
+var hand_vards_array = []
+# RNG class
 var rng = RandomNumberGenerator.new()
 
+# Class methods
 func change_resource_points(points):
 	self.resource_points += points
 

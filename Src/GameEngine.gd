8ce6055a23d49510d3dfbfed3d288b8989735b44
Johan <51072862+johan1954@users.noreply.github.com>
2023-02-04 22:55:53 +0200

    Change Shroom attack logic

--- Src/GameEngine.gd
+++ Src/GameEngine.gd
@@ -1,6 +1,5 @@
 extends Node
 
-
 func _unhandled_input(event):
 	if event is InputEventKey:
 		if event.pressed and event.keycode == KEY_ESCAPE:
@@ -12,12 +11,11 @@ func _unhandled_input(event):
 			print(get_tree().get_current_scene()) 
 			#get_tree().change_scene_to_file("res://MainMenu.tscn")
 
-
 var map_generator : MapGenerator
 
-const CAPTURE_THRESHOLD = 20
+const CONTROL_TIME = 20
 
-var player_growth_speed = 10
+var player_growth_speed = 30
 var enemy_growth_speed = -5
 
 var player_damage_output = 0
@@ -27,13 +25,13 @@ func start_game():
 	map_generator = MapGenerator.new()
 	map_generator.generate_map()
 
-	var new_card_button = AssetsPreload.CARD_BUTTON_NODE.instantiate()
-	Map.add_child(new_card_button)
-	new_card_button = AssetsPreload.CARD_BUTTON_NODE.instantiate()
-	new_card_button.message = "Test 1"
-	new_card_button.console_print()
-	Map.add_child(new_card_button)
-	new_card_button.position = Vector2(-50, -50)
-	new_card_button.message = "Test 2"
-	new_card_button.console_print()
+#	var new_card_button = AssetsPreload.CARD_BUTTON_NODE.instantiate()
+#	Map.add_child(new_card_button)
+#	new_card_button = AssetsPreload.CARD_BUTTON_NODE.instantiate()
+#	new_card_button.message = "Test 1"
+#	new_card_button.console_print()
+#	Map.add_child(new_card_button)
+#	new_card_button.position = Vector2(-50, -50)
+#	new_card_button.message = "Test 2"
+#	new_card_button.console_print()
 	

b333f0181fb5e1f5658e82ee05b2265aa74499a3
Juuso Ylikoski <jousimies2@gmail.com>
2023-02-05 14:29:00 +0200

    Add text to cards

--- Src/AssetsPreload.gd
+++ Src/AssetsPreload.gd
@@ -6,3 +6,7 @@ var PLAYER_SHROOM_NODE = preload("res://Src/Objects/PlayerShroom.tscn")
 var ENEMY_SHROOM_NODE = preload("res://Src/Objects/EnemyShroom.tscn")
 var SOUND = preload("res://Audio/FGJ23_Shroom_rts_transition.wav")
 var CARD_BUTTON_NODE = preload("res://Src/UI/buttons/CardButton.tscn")
+
+var CARD_GF1 = preload("res://GFX Assets/Base_1small.png")
+var CARD_GF2 = preload("res://GFX Assets/Base_2.png")
+
--- Src/UI/buttons/CardButtonManager.gd
+++ Src/UI/buttons/CardButtonManager.gd
@@ -7,6 +7,11 @@ var card_buttons: Array[CardButton] = []
 func new_button(card : CardBase, card_manager : CardManager):
 	var new_card_button = AssetsPreload.CARD_BUTTON_NODE.instantiate()
 	new_card_button.card_object = card
+	if card is CardAddAttack1:
+		new_card_button.texture_normal = AssetsPreload.CARD_GF2
+	else:
+		new_card_button.texture_normal = AssetsPreload.CARD_GF1
+	new_card_button.get_child(0).text = card.title + "\n" + card.descrition;
 	new_card_button.card_manager = card_manager
 	card_buttons.append(new_card_button)
 	add_child(new_card_button)

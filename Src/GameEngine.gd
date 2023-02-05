6654eaecf1de8d74103746b0f0963a44eda4f15c
AK-uni-git <55245797+AK-uni-git@users.noreply.github.com>
2023-02-05 14:58:02 +0200

    Aku k (#37)
    * Card button demo
    
    * Another button test
    
    * Initial proto for settings menu and volume slider.
    
    * Volume slider improvements.
    
    * Camera zoom limiter and made esc to quit game.
    
    * Made the music loop.
    
    * Easter egg.
    
    * Confirmation dialog for exiting game.

--- Src/GameEngine.gd
+++ Src/GameEngine.gd
@@ -1,19 +1,7 @@
 extends Node
 
 #func _ready():
-#	AudioServer.playback_speed_scale = 2.0
-
-func _unhandled_input(event):
-	if event is InputEventKey:
-		if event.pressed and event.keycode == KEY_ESCAPE:
-			GlobalAudio.playSoundEffect()
-			var map = get_node("/root/Map")
-			for child in map.get_children():
-				child.queue_free()
-			
-			print("Kiitos ohjelman käytöstä!")
-			get_tree().quit()
-#			get_tree().change_scene_to_file("res://MainMenu.tscn")
+#	AudioServer.playback_speed_scale = 5.0
 
 var map_generator : MapGenerator
 var map_state : MapState
@@ -30,6 +18,37 @@ var enemy_damage_output = 10
 var _main_volume_bus := AudioServer.get_bus_index("Master")
 var _music_volume_bus := AudioServer.get_bus_index("Music")
 var _sfx_volume_bus := AudioServer.get_bus_index("SFX")
+
+func alert(alert_text: String, title: String='Message') -> void:
+	var dialog = AcceptDialog.new()
+	dialog.dialog_text = alert_text
+	dialog.add_cancel_button ("Cancel")
+	dialog.title = title
+	dialog.ok_button_text = "Yes"
+	dialog.set_script(load("res://Src/UI/scripts/exitGameConfirmationDialog.gd"))
+	dialog.connect("confirmed", Callable(dialog,'delete_all'))
+	add_child(dialog,true)
+	dialog.popup_centered()
+
+
+
+func _unhandled_input(event):
+	if event is InputEventKey:
+		#print("InputEventKey ar den ", event.keycode)
+		if event.pressed and event.keycode == KEY_ESCAPE:
+			alert("Are you sure you want to quit game", "Quit game?")
+			#get_tree().quit()
+#			get_tree().change_scene_to_file("res://MainMenu.tscn")
+
+		#Easter eggs. Speed up music volume and increase volume
+		elif event.pressed and event.keycode == 39:
+			AudioServer.playback_speed_scale = 5.0
+		elif event.pressed and event.keycode == 96:
+			AudioServer.set_bus_volume_db(_music_volume_bus, linear_to_db(20))
+		elif event.pressed and event.keycode == 93:
+			#Remove easter egg effectsd
+			AudioServer.playback_speed_scale = 1.0
+			AudioServer.set_bus_volume_db(_music_volume_bus, linear_to_db(1))
 
 
 
